216552c5c4c2ec9e564ca8dc9b8480cc45b398e4
Tiago Costa <tiagodpv@gmail.com>
2023-07-14 21:18:57 -0400

    test 1

--- 01/2622-python-tdd-57de629597dabe71ad125067e9dbedde5babe577/codigo/main.py
+++ 01/2622-python-tdd-57de629597dabe71ad125067e9dbedde5babe577/codigo/main.py
@@ -1,5 +1,12 @@
 from bytebank import Funcionario
 
-lucas = Funcionario('Lucas Carvalhoo', '13/03/2000', 1000)
+#lucas = Funcionario('Lucas Carvalhoo', '13/03/2000', 1000)
 
-print(lucas.idade())
+#print(lucas.idade())
+
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '13/03/2000', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+
+teste_idade()
